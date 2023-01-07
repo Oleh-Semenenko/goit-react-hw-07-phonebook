6fbbefd492688cbfa9dcc5e9d9925bf1ccf80c49
Oleh Semenenko <o.semenenko.mar@gmail.com>
2023-01-07 14:45:42 +0200

    optimized selectors and sorted contacts

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,24 +1,14 @@
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilter } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 import { Contact } from 'components/Contact/Contact';
 import { Contacts } from './ContactList.styled';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectFilter);
-
-  function getVisibleContacts() {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact => 
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-  }
-
-  const filteredContacts = getVisibleContacts();
+  const contacts = useSelector(selectVisibleContacts);
 
   return (
     <Contacts>
-      {filteredContacts.map(({ id, name, phone }) => {
+      {contacts.map(({ id, name, phone }) => {
         return <Contact key={id} id={id} name={name} phone={phone} />;
       })}
     </Contacts>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 
 export const selectIsLoading = state => state.contacts.isLoading;
@@ -5,3 +7,17 @@ export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 
 export const selectFilter = state => state.filter;
+
+export const selectVisibleContacts = createSelector(
+  [selectFilter, selectContacts],
+  (filter, contacts) => {
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return filteredContacts.sort(function (a, b) {
+      return b.id - a.id;
+    });
+  }
+);
